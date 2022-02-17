17b551847c7e36ab2cc634ceab41ad51384e673d
bkd42 <riveramanuelh@gmail.com>
2022-02-17 14:42:27 -0600

    first commit

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,3 +1,29 @@
+import type { GetServerSideProps } from 'next';
+import { prisma } from '@/backend/utils/prisma';
+import { AsyncReturnType } from '@/utils/ts-bs';
+
+import Image from 'next/image';
+import Head from 'next/head';
+
+const getPokemonInOrder = async () => {
+  return await prisma.pokemon.findMany({
+    orderBy: {
+      VoteFor: { _count: 'desc' },
+    },
+    select: {
+      id: true,
+      name: true,
+      spriteUrl: true,
+      _count: {
+        select: {
+          VoteFor: true,
+          VoteAgainst: true,
+        },
+      },
+    },
+  });
+};
+
 function About() {
   return <div>About</div>;
 }
