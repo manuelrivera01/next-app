36d499ed21a927ae51b4010618c9cfeaadf8a71c
bkd42 <riveramanuelh@gmail.com>
2022-02-17 15:12:51 -0600

    first commit

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -34,8 +34,66 @@ const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
   return (VoteFor / (VoteFor + VoteAgainst)) * 100;
 };
 
-function About() {
-  return <div>About</div>;
-}
+const PokemonListing: React.FC<{
+  pokemon: PokemonQueryResult[number];
+  rank: number;
+}> = ({ pokemon, rank }) => {
+  return (
+    <div className='relative flex border-b p-2 items-center justify-between'>
+      <div className='flex items-center'>
+        <div className='flex items-center pl-4'>
+          <Image
+            src={pokemon.spriteUrl}
+            width={64}
+            height={64}
+            layout='fixed'
+          />
+          <div className='pl-2 capitalize'>{pokemon.name}</div>
+        </div>
+      </div>
+      <div className='pr-4'>
+        {generateCountPercent(pokemon).toFixed(2) + '%'}
+      </div>
+      <div className='absolute top-0 left-0 z-20 flex items-center justify-center px-2 font-semibold text-white bg-gray-600 border border-gray-500 shadow-lg rounded-br-md'>
+        {rank}
+      </div>
+    </div>
+  );
+};
 
-export default About;
+const ResultsPage: React.FC<{
+  pokemon: PokemonQueryResult;
+}> = (props) => {
+  return (
+    <div className='flex flex-col items-center'>
+      <Head>
+        <title>Roundest Pokemon Results</title>
+      </Head>
+      <h2 className='text-2xl p-4'>Results</h2>
+      <div className='flex flex-col w-full max-w-2xl border'>
+        {props.pokemon
+          .sort((a, b) => {
+            const difference =
+              generateCountPercent(b) - generateCountPercent(a);
+
+            if (difference === 0) {
+              return b._count.VoteFor - a._count.VoteFor;
+            }
+
+            return difference;
+          })
+          .map((currentPokemon, index) => {
+            return (
+              <PokemonListing
+                pokemon={currentPokemon}
+                key={index}
+                rank={index + 1}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default ResultsPage;
