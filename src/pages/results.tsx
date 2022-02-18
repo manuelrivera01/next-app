7ae2b5de72f263197cafe9c049e9916eca3a219e
bkd42 <riveramanuelh@gmail.com>
2022-02-18 10:29:39 -0600

    first commit

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -4,35 +4,6 @@ import { AsyncReturnType } from '@/utils/ts-bs';
 import Image from 'next/image';
 import Head from 'next/head';
 
-const getPokemonInOrder = async () => {
-  return await prisma.pokemon.findMany({
-    orderBy: {
-      VoteFor: { _count: 'desc' },
-    },
-    select: {
-      id: true,
-      name: true,
-      spriteUrl: true,
-      _count: {
-        select: {
-          VoteFor: true,
-          VoteAgainst: true,
-        },
-      },
-    },
-  });
-};
-
-type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
-
-const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
-  const { VoteFor, VoteAgainst } = pokemon._count;
-  if (VoteFor + VoteAgainst === 0) {
-    return 0;
-  }
-  return (VoteFor / (VoteFor + VoteAgainst)) * 100;
-};
-
 function About() {
   return <div>About</div>;
 }
